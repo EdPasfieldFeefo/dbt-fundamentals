93914fe6d6be975f7d24b265e68066ea31ff39ef
Edward Pasfield <edward.pasfield@gmail.com>
2022-04-06 13:37:02 +0000

    sources updated for customers/orders/payments

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,13 +1,9 @@
-{{ config(
-    materialized="table"
-)}}
-
 with customers as (
     select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
